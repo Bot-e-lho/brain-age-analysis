75df68cc62ffe84f24fc21c5dc518662c1cec276
spc <pedballester@gmail.com>
2020-07-13 16:41:44 -0400

    differences sz

--- analysis.R
+++ analysis.R
@@ -1,8 +1,9 @@
 library(meta)
-matrix <- read.csv('/home/ballester/Projects/brain-age-meta-analysis/database.csv')
+matrix <- read.csv('/home/spc/Projects/brain-age-meta-analysis/database.csv')
 
-r = metacont(matrix$N_SZ, matrix$E_SZ, matrix$S_SZ, matrix$N_HC, matrix$E_HC, matrix$S_HC, matrix$Study)
+matrix_sz = matrix[!is.na(matrix$N_SZ),]
+r = metacont(matrix_sz$N_SZ, matrix_sz$E_SZ, matrix_sz$S_SZ, matrix_sz$N_HC, matrix_sz$E_HC, matrix_sz$S_HC, matrix_sz$Study)
 forest.meta(r)
 
-r = metacont(matrix$N_BD, matrix$E_BD, matrix$S_BD, matrix$N_HC, matrix$E_HC, matrix$S_HC, matrix$Study)
-forest.meta(r)
+#r = metacont(matrix$N_BD, matrix$E_BD, matrix$S_BD, matrix$N_HC, matrix$E_HC, matrix$S_HC, matrix$Study)
+#forest.meta(r)
