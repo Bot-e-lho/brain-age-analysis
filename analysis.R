447395c0f4b6d82a1f960a3bc6d9da0f6c6517da
spc <pedballester@gmail.com>
2020-07-21 17:15:04 -0400

    updates to the meta-analysis

--- analysis.R
+++ analysis.R
@@ -3,7 +3,10 @@ matrix <- read.csv('/home/spc/Projects/brain-age-meta-analysis/database.csv')
 
 matrix_sz = matrix[!is.na(matrix$N_SZ),]
 r = metacont(matrix_sz$N_SZ, matrix_sz$E_SZ, matrix_sz$S_SZ, matrix_sz$N_HC, matrix_sz$E_HC, matrix_sz$S_HC, matrix_sz$Study)
+#pdf("/home/spc/Projects/brain-age-meta-analysis/images/sz.pdf")
 forest.meta(r)
+#dev.off()
 
-#r = metacont(matrix$N_BD, matrix$E_BD, matrix$S_BD, matrix$N_HC, matrix$E_HC, matrix$S_HC, matrix$Study)
+matrix_bd = matrix[!is.na(matrix$N_BD),]
+r = metacont(matrix_bd$N_BD, matrix_bd$E_BD, matrix_bd$S_BD, matrix_bd$N_HC, matrix_bd$E_HC, matrix_bd$S_HC, matrix_bd$Study)
 #forest.meta(r)
---
+++ confidence_to_std.R
@@ -0,0 +1,15 @@
+CI_lower = 1.96*std + mean
+
+n = 120
+mean = 2.64
+CI = 3.48
+std = ((CI - mean)/1.96) * sqrt(n)
+std_fep = std
+CI_lower_fep = mean - 1.96*std
+
+n = 114
+mean = 0.14
+CI = 1.00
+std = ((CI - mean)/1.96) * sqrt(n)
+std_hc = std
+CI_lower_hc = mean - 1.96*std
