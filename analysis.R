d1ff33e5df8fcf59d442e8c034d23db977c39557
spc <pedballester@gmail.com>
2020-07-22 09:58:08 -0400

    last database version

--- analysis.R
+++ analysis.R
@@ -4,9 +4,13 @@ matrix <- read.csv('/home/spc/Projects/brain-age-meta-analysis/database.csv')
 matrix_sz = matrix[!is.na(matrix$N_SZ),]
 r = metacont(matrix_sz$N_SZ, matrix_sz$E_SZ, matrix_sz$S_SZ, matrix_sz$N_HC, matrix_sz$E_HC, matrix_sz$S_HC, matrix_sz$Study)
 #pdf("/home/spc/Projects/brain-age-meta-analysis/images/sz.pdf")
-forest.meta(r)
+#forest.meta(r)
 #dev.off()
 
 matrix_bd = matrix[!is.na(matrix$N_BD),]
 r = metacont(matrix_bd$N_BD, matrix_bd$E_BD, matrix_bd$S_BD, matrix_bd$N_HC, matrix_bd$E_HC, matrix_bd$S_HC, matrix_bd$Study)
 #forest.meta(r)
+
+matrix_mdd = matrix[!is.na(matrix$N_MDD),]
+r = metacont(matrix_mdd$N_MDD, matrix_mdd$E_MDD, matrix_mdd$S_MDD, matrix_mdd$N_HC, matrix_mdd$E_HC, matrix_mdd$S_HC, matrix_mdd$Study)
+forest.meta(r)
