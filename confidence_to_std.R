03a614dfc841f2f713dba1aba168d047b09f4d69
spc <pedballester@gmail.com>
2020-07-22 10:37:01 -0400

    update van gastel

--- confidence_to_std.R
+++ confidence_to_std.R
@@ -1,15 +1,34 @@
-CI_lower = 1.96*std + mean
+calculate_std = function (CI, mean, n, name) {
+  std = ((CI - mean)/1.96) * sqrt(n)
+  CI_lower = mean - 1.96*(std/sqrt(n))
+  print(c(name, std, CI_lower))
+}
+#CI_lower = 1.96*std + mean
 
 n = 120
 mean = 2.64
 CI = 3.48
-std = ((CI - mean)/1.96) * sqrt(n)
-std_fep = std
-CI_lower_fep = mean - 1.96*std
+calculate_std(CI, mean, n, "Kolenic SZ")
 
 n = 114
 mean = 0.14
 CI = 1.00
-std = ((CI - mean)/1.96) * sqrt(n)
-std_hc = std
-CI_lower_hc = mean - 1.96*std
+calculate_std(CI, mean, n, "Kolenic HC")
+
+### VAN GESTEL ###
+n = 43
+mean = 4.96
+CI = 6.64
+calculate_std(CI, mean, n, "Van Gestel Non-Lithium")
+
+n = 41
+mean = 0.83
+CI = 2.54
+calculate_std(CI, mean, n, "Van Gestel Lithium")
+
+n = 45
+mean = -0.15
+CI = 1.50
+calculate_std(CI, mean, n, "Van Gestel HC")
+
+### VAN GESTEL ###
